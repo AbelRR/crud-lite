c6409fd956d19bbe976fbfe55888d3ee015f2025
AbelRR <reyesregalado@gmail.com>
2018-07-20 11:29:17 -0700

    added new feature and gitIgnore

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ $(document).ready(function(){
 //		
 //	})
 	
-		$().on('click', function(){
-			
+		$('.store-btn').on('click', function(event){
+			localStorage.setItem('hrext', 'three is the best');
+		})
+	
+		$('.get-btn').on('click', function(event){
+			localStorage.getItem('hrext');
+		})
+	
+		$('.delete-btn').on('click', function(event){
+			localStorage.removeItem('hrext');
 		})
 })
 
