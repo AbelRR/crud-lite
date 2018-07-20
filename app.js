409bfe1415b19d2f0d43a739b8f796d86a103b6a
AbelRR <reyesregalado@gmail.com>
2018-07-20 11:27:55 -0700

    Local Storage, Add, Remove

---
+++ app.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+	
+
+// attach event listener to buttons (input?)
+	
+// create function stub for read/write/delete
+	
+//	$().on('click', function(){
+//		
+//	})
+	
+		$().on('click', function(){
+			
+		})
+})
+
+
+
+
+
+
+
+
+
+
