25dab3915f0be10482f212bbd985e9605e26f5c9
AbelRR <reyesregalado@gmail.com>
2018-07-24 20:53:05 -0700

    Added Grid layout and basic styling.

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ console.log('+++ filename: app.js: jquery.ready expected no error actual: ', $ )
 window.streams = {}; 
 streams.home = [];
 streams.customers = []
-	var parsedCustomerData = JSON.parse(localStorage.getItem("customerData"));
-streams.orders = []; 
-	var parsedOrdersData = JSON.parse(localStorage.getItem("ordersData"));
-
+streams.orders = [];
 	
+// customers
 const customers = streams.customers;
-const orders = streams.orders; 
-const customerForm = document.querySelector('.add-client');
-const customerList = document.querySelector('.clientList')
+const customerForm = document.querySelector('.addClient');
+const customerList = document.querySelector('.clientList');
+const parsedCustomerData = JSON.parse(localStorage.getItem("customerData"));
 	
+// orders
+const orders = streams.orders; 
 const orderForm = document.querySelector('.add-order');
 const orderList = document.querySelector('.orderList')	
+const parsedOrderData = JSON.parse(localStorage.getItem("orderData"));
 
 ////////////////////////////////** ADDING CUSTOMERS **////////////////////////////////////////
 function addCustomer(ele){
@@ -28,11 +29,18 @@ function addCustomer(ele){
 	const customer = {name, phoneNumber, email, notes};
 
 	customers.push(customer);
-	localStorage.setItem("customerData", JSON.stringify(customers))
+	localStorage.setItem("customerData", JSON.stringify(customers));
+	(function (customers = [], customerList) {
+	// update customers and orders 
+		customerList.innerHTML = customers.map((customer) => {
+			return `<li>Client Name: ${customer.name} || Phone Number: ${customer.phoneNumber} || Email: ${customer.email} || Notes: ${customer.notes}</li>`
+		})
+	})(customers,customerList);
 	this.reset();
 }
 
 customerForm.addEventListener('submit', addCustomer);
+	
 /////////////////////////////////////** ADDING ORDERS **//////////////////////////////////////////
 function addOrder(ele){
 	ele.preventDefault();
@@ -47,24 +55,42 @@ function addOrder(ele){
 	const order = {customerName, date, address, tables, chairs, deliveryTime, pickupTime}
 
 	orders.push(order);
-	localStorage.setItem("ordersData", JSON.stringify(orders))
+	localStorage.setItem("orderData", JSON.stringify(orders));
+	(function (orders = [], ordersList) {
+	// update customers and orders 
+		ordersList.innerHTML = orders.map((order) => {
+			return `<li>On ${order.date}, ${order.customerName} will want ${order.tables} tables and ${order.chairs} chairs.<br> Address: ${order.address}...<br> Delivery at: ${order.deliveryTime} || Pickup at: ${order.pickupTime}</li>`
+		})
+	})(orders,orderList);
 	this.reset();
 }
 
 orderForm.addEventListener('submit', addOrder);
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-function populateClients(itemsArr = [], itemsList){
-	itemsArr.forEach(customer => {
-		localStorage.setItem("customerData", JSON.stringify(customers))
-	});
-//	itemsList.innerHTML = itemsArr.map((customer, index) => {
-	
-//	**ORDER STRING**	return `<li id="order${index}">On ${order.date}, ${order.customerName} will want ${order.tables} tables and ${order.chairs} chairs.<br> Address: ${order.address}...<br> Delivery at: ${order.deliveryTime} || Pickup at: ${order.pickupTime}</label></li>`
-//	**CUSTOMER STRING**	return `<li id="customer${index}">Client Name: ${customer.name} || Phone Number: ${customer.phoneNumber} || Email: ${customer.email} || Notes: ${customer.notes}</li> `
-//	})
-}
 
+
+//function populateData(itemsArr = [], itemsList, iterator){
+////	itemsArr.forEach(customer => {
+////		localStorage.setItem("customerData", JSON.stringify(customers))
+////	});
+//	itemsList.innerHTML = iterator(itemsArr);
+//}
+//	
+//	populateData(customers, customerList, function(customers){
+//		customerList.innerHTML = customers.map((customer) => {
+//			return `<li>Client Name: ${customer.name} || Phone Number: ${customer.phoneNumber} || Email: ${customer.email} || Notes: ${customer.notes}</li> `
+//		})
+//	})
+//	
+//	populateData(orders, orderList, function(orders){
+//		orderList.innerHTML = orders.map((order, index) => {
+//			return `<li id="order${index}">On ${order.date}, ${order.customerName} will want ${order.tables} tables and ${order.chairs} chairs.<br> Address: ${order.address}...<br> Delivery at: ${order.deliveryTime} || Pickup at: ${order.pickupTime}</label></li>`
+//		});
+//	})
+
+
+		
 
 
 });
